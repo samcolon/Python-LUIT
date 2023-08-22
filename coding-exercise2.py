bda4260eb5b4612e4cce2e5f2aaad549c4696a6d
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-22 14:25:00 -0400

    uploaded coding-exercise2.py

---
+++ coding-exercise2.py
@@ -0,0 +1,13 @@
+# Ask the user to provide their login and native language. Use the following prompts:
+# Enter your login: << remember to add a space at the end of this prompt!
+# Enter your native language: << remember to add a space at the end of this prompt!
+# Then, show the user the following message:
+# Your login is {login provided} and you speak {language provided}
+# For example, if the user provides the login h_potter and language British English, show:
+# Your login is h_potter and you speak British English
+# Watch out for typos: you must show the output in this particular format!
+
+
+login = input('Enter your login: ')
+language = input('Enter your native language: ')
+print('Your login is', login, 'and you speak', language)
