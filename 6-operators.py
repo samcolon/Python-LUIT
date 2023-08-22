443d1146d8a7c2e3a2a547fc86696ae8fe05cdb5
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-22 11:59:43 -0400

    uploaded 6-operators.py

---
+++ 6-operators.py
@@ -0,0 +1,24 @@
+# addition
+2 + 3
+
+# subtraction
+5 - 4
+
+# multiplication
+3 * 5
+
+# division /  //   %
+# standard division
+6 / 2
+7 / 2 
+
+# integer division (returns nearest whole number)
+6 // 2
+7 // 2
+
+# modulus division (shows the remainder)
+6 % 2
+7 % 2
+
+# power operator (to the power)
+2 ** 3
