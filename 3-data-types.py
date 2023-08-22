a594b99bcad81380058f6cba4d5ec3ce5dc3c9db
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-22 11:05:17 -0400

    uploaded 3-data-types.py

---
+++ 3-data-types.py
@@ -0,0 +1,14 @@
+### string
+greeting = 'Hello, friend'
+
+### integer
+age = 35
+
+### float
+speed1 = 4.5
+speed2 = 4.0
+speed3 = 4.
+
+### boolean (True is equivalaent to number 1 False is equivalent to number 0)
+am_i_ugly = True
+am_i_the_best = False
