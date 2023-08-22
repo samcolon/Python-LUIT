0e46f1fed9027e17ea48384b41a4fe532137fb50
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-22 13:59:41 -0400

    uploaded 7-reassigning-values.py

---
+++ 7-reassigning-values.py
@@ -0,0 +1,27 @@
+age = 28
+print(age)
+
+age = 28
+new_age = age + 5
+print(new_age)
+
+age = 28
+age = age + 7 
+print(age)
+
+age = 28
+age += 7
+print(age)
+
+age *=2 # current value times 2
+age -= 5 # current value minus 5
+age /= 3 # current value divided by 3
+
+text = 'hokus' + 'pokus'
+print(text)
+
+print('hokus' * 5)
+
+print(23+5) # equals 28
+print('23' + '5') # equals 235 (concatenated together)
+
