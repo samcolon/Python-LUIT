0acb5b662c617de46e728aaa56dd751472687b7a
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-23 13:00:54 -0400

    uploaded 10-more-print-and-strings.py

---
+++ 10-more-print-and-strings.py
@@ -0,0 +1,17 @@
+# keyword arguments/named arguments
+
+#example:
+print('Hello, World!')
+print('Python speaking!')
+
+#keyword argument:
+print('Hello, World!', end='.')
+print('Python speaking!')
+
+#example:
+first_name = 'John'
+print('Your first name is', first_name, 'Welcome!')
+
+#sep argument:
+first_name = 'John'
+print('Your first name is', first_name, 'Welcome!', sep='-')
