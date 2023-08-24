5bd18e12d8201bbe4d73cf64520fdc698759ef22
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-24 15:38:04 -0400

    uploaded 14-nested-if.py

---
+++ 14-nested-if.py
@@ -0,0 +1,17 @@
+answer_a =  input('Do you like travelling? y/n: ')
+if answer_a == 'y':   
+   print('Good!')
+else:
+    print('Sorry to hear that!')
+    
+
+
+answer_a =  input('Do you like travelling? y/n: ')
+if answer_a == 'y':
+   answer_b = input('And do you like Asia? y/n: ')
+   if answer_b == 'y':
+       print('Excellent! You can win aa ticket to Thailand')
+   else:
+       print('Sorry to hear that')
+else:
+    print('Sorry to hear that!')
