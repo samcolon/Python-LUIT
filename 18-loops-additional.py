312f46437038dc8c49cda46da24f7ad729c34593
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-25 11:41:01 -0400

    uploaded 18-loops-additional.py

---
+++ 18-loops-additional.py
@@ -0,0 +1,23 @@
+for i  in range(11):
+    pass
+
+
+for a in range(1,6):
+    for b in range(1,6):
+        print(a, 'x', b, '=', a * b)
+        
+    
+i = 5
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print('else:', i)
+    
+    
+i = 2
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print('else:', i) 
