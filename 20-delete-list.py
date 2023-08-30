eadd0f54e8839de9182f8e13caa744142bec372a
samcolon <116681627+samcolon@users.noreply.github.com>
2023-08-30 12:19:01 -0400

    uploaded 20-delete-list.py

---
+++ 20-delete-list.py
@@ -0,0 +1,8 @@
+top_cities = ['New York City', 'Los Angeles','Singapore', 'Chicago', 'Houston', 'Phoenix']
+del top_cities[2]
+print(top_cities)
+
+top_cities = ['New York City', 'Los Angeles','Singapore', 'Chicago', 'Houston', 'Phoenix']
+del top_cities[3:]
+print(top_cities)
+#output is ['New York City, 'Los Angeles, 'Signapore']
