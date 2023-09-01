c3be856a8e89826ebd83f695fc1ba089e9d70363
samcolon <116681627+samcolon@users.noreply.github.com>
2023-09-01 10:40:01 -0400

    uploaded 34-dict-operations.py

---
+++ 34-dict-operations.py
@@ -0,0 +1,33 @@
+grades = {}
+grades['John'] = 'A-'
+grades['Anne'] = 'B'
+print(grades)
+
+grades['Anne'] = 'A'
+print(grades)
+
+grades.update({'John':'A'})
+print(grades)
+
+print(len(grades))
+
+if 'John' in grades:
+    print('John got:', grades['John'])
+    
+del grades['John']
+print(grades)
+
+grades = {}
+grades['John'] = 'A-'
+grades['Anne'] = 'B'
+
+for el in grades:
+    print(el)
+for el in grades.keys():
+    print(el)
+    
+for el in grades.values():
+    print(el)
+    
+for person, grade in grades.items():
+    print(person, 'got', grade)
