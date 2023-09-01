72749e04ba4915d0f9dfe4236c6565aa7b6694e4
samcolon <116681627+samcolon@users.noreply.github.com>
2023-09-01 10:28:38 -0400

    uploaded 33-intro-dict.py

---
+++ 33-intro-dict.py
@@ -0,0 +1,14 @@
+emails = {
+    'Anne Stahl': 'astahl@gmail.com',
+    'Peter Small': 'peters@yandex.com',
+    'Mark Steel': 'mark@steel.com'
+}
+print(emails['Mark Steel'])
+
+spanish_animals = {
+   'dog': 'el perro',
+   'cat': 'el gato',
+   'horse': 'el caballo',
+   'bird': 'el pajaro' 
+}
+print(spanish_animals['bird'])
