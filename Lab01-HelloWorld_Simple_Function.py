76233e6887f76764ef6577cfad771ae13973c0e6
samcolon <116681627+samcolon@users.noreply.github.com>
2023-09-08 13:25:50 -0400

    updated HelloWorld_Simple_Function.py

--- Lab01-HelloWorld_Simple_Function.py
+++ Lab01-HelloWorld_Simple_Function.py
@@ -1,6 +1,7 @@
-# A function that prints hello world
+# A function that returns hello world
 def hello_world():
-    print('hello world')
+    return 'hello world'
 
-# This line calls (runs) the function
-hello_world()
+# Assign the hello_world() function to a variable.
+greeting = hello_world()
+print(greeting)
